9ad47f12a8a5d91b9e2178eb617e55382dd9820b
kozolex <kozolex@gmail.com>
2024-07-30 16:04:37 +0200

    ok

--- src/bootcamp_chat_backend/src/lib.rs
+++ src/bootcamp_chat_backend/src/lib.rs
@@ -1,13 +1,14 @@
 use std::{cell::{Ref, RefCell}, collections::HashMap};
+use candid::Principal
 
 
 thread_local! {
-    static NOTES: RefCell<Vec<String>> = RefCell::default();
+    static NOTES: RefCell<HashMap<Principal, Vec<String>>> = RefCell::default();
 }
 
 #[ic_cdk::query]
-fn get_notes() -> Vec<String> {
-    NOTES.with_borrow(|notes| notes.clone())
+fn get_notes(user: Principal) -> Vec<String> {
+    NOTES.with_borrow(|notes| notes.get(user).clone())
 }
 
 #[ic_cdk::update]
